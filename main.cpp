8a0c1f768a9298b121276fbe06d5f4b404fe5311
KaoruMizugaki <k021g1249@g.neec.ac.jp>
2022-12-02 03:33:02 +0900

    表示の機能追加

--- main.cpp
+++ main.cpp
@@ -3,19 +3,26 @@
 #include "time.h"
 #include <windows.h>
 #include <functional>
+#include <list>
+#include <iostream>
+using namespace std;
 
 int main() {
 
-	int a = 0;
+	std::list<int> house = { 21,52,43 };
+	std::list<int>::iterator it;
 
-	while (true) {
+	int Check = 0;
+
+	while (true)
+	{
 
 		printf("[要素の操作]\n");
 		printf("1.要素の表示\n");
 		printf("2.要素の挿入\n");
 
-		if (false) {
-			printf("3.[要素の編集\n");
+		if (house.size() != 0) {
+			printf("3.要素の編集\n");
 			printf("4.要素の削除\n");
 		}
 
@@ -25,10 +32,74 @@ int main() {
 
 		printf("操作を選択してください\n");
 
-		scanf_s("%d\n",&a);
+		scanf_s("%d", &Check);
+
+		if (Check == 1)
+		{
+
+			printf("\n[要素の表示]\n");
+			printf("1.要素の一覧表示\n");
+			printf("2.順番を指定して要素を表示\n\n");
+
+			printf("操作を選択してください\n");
+
+			scanf_s("%d", &Check);
+
+			if (Check == 1)
+			{
+
+				printf("要素一覧:{\n");
+				for (auto itr = house.begin(); itr != house.end(); ++itr)
+				{
+					//it = house[i];
+
+					//printf("%d", itr);
+					std::cout << *itr << "\n";
+
+					//printf("%d:%d",i,house.[i]);
+				}
+
+				printf("}\n\n");
+
+				printf("要素数:%d\n", (int)house.size());
+
+				printf("\n-----\n");
+			}
+			else if (Check == 2)
+			{
+				printf("順番を指定して要素を表示\n\n");
+
+				printf("表示したい要素の順番を指定してください。\n");
+
+				scanf_s("%d", &Check);
+
+				auto itr = house.begin();
+
+				for (int i = 1; i < Check; i++)
+				{
+					itr++;
+				}
+
+				std::cout << *itr << "\n";
+
+			}
+		}
+		else if (Check == 2)
+		{
+			for (auto itr = house.begin(); itr != house.end(); ++itr)
+			{
+				//it = house[i];
+
+				std::cout << *itr << "\n";
+
+				//printf("%d:%d",i,house.[i]);
+			}
+		}
+
 	}
 
 	system("pause");
 
 	return 0;
+
 }
