14f917a4bd2f5480a334434508d3bf75e6ca73ce
KaoruMizugaki <k021g1249@g.neec.ac.jp>
2023-02-06 06:25:35 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -8,203 +8,105 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
+	list<string> station = {
+	"oosaki","gotanda","meguro","ebisu","sibuya",
+	"harajuku","yoyogi","sinjuku","sinookubo","takadanobaba",
+	"meziro","ikebukuro","ootuka","sugamo","komagome",
+	"tabata","nisinippori","nippori","oukoku","ueno",
+	"okatimati","akihabara","kanda","tokyo","yuurakutyou",
+	"sinbasi","hamamatutyou","tamati","takanawagw-towei","sinagawa" };
 
-	list<int> house = { 1,2,3 };
-	list<int>::iterator it;
+	list<string>::iterator it;
 
-	int Check = 0;
-	int Mode = 0;
+	int check = 0;
 
 	while (true)
 	{
-		if (Check == 0)
+		printf("どの年代の山手線を表示しますか。\n");
+		printf("数字を入力してください。\n\n");
+		printf("1.1970年\n");
+		printf("2.2019年\n");
+		printf("3.2022年\n\n");
+
+		scanf_s("%d", &check);
+
+		if (check == 1)
 		{
-			printf("[要素の操作]\n");
-			printf("1.要素の表示\n");
-			printf("2.要素の挿入\n");
+			printf("1970年\n");
+			printf("\n");
 
-			if (house.size() != 0) {
-				printf("3.要素の編集\n");
-				printf("4.要素の削除\n");
-			}
-
-			printf("-----------\n");
-
-			printf("操作を選択してください\n");
-
-			scanf_s("%d", &Check);
-		}
-		else if (Check == 1)
-		{
-			printf("\n[要素の表示]\n");
-			printf("1.要素の一覧表示\n");
-			printf("2.順番を指定して要素を表示\n");
-			printf("9.要作操作にもどる\n\n");
-
-			printf("操作を選択してください\n");
-
-			scanf_s("%d", &Mode);
-
-			if (Mode == 1)
+			for (auto itr = station.begin(); itr != station.end(); ++itr)
 			{
-				int a = 0;
-				printf("\n要素一覧:{\n");
-				for (auto itr = house.begin(); itr != house.end(); ++itr)
+				if (*itr == "nisinippori")
 				{
-					printf("%d:", a);
-
-					a++;
-
-					std::cout << *itr << "\n";
+					station.erase(station.begin() + itr);
+					continue;
 				}
 
-				printf("}\n\n");
-
-				printf("要素数:%d\n", (int)house.size());
-			}
-			else if (Mode == 2)
-			{
-				printf("順番を指定して要素を表示\n\n");
-
-				printf("表示したい要素の順番を指定してください。\n");
-
-				scanf_s("%d", &Mode);
-
-				auto itr = house.begin();
-
-				for (int i = 1; i < Mode; i++)
+				if (*itr == "takanawagw-towei")
 				{
-					itr++;
-
-					if (i = Mode - 1)
-						printf("\n%d:", i);
+					station.erase(itr);
+					continue;
 				}
 
 				std::cout << *itr << "\n";
 			}
-			else if (Mode == 9)
-			{
-				printf("\n");
-				continue;
-			}
 
-			printf("\n-----\n");
-
-			printf("1.要素の表示に戻る\n");
-			printf("2.要素の操作に戻る\n");
-
-			scanf_s("%d", &Check);
-
-			if (Check == 2)
-			{
-				Check = 0;
-			}
+			printf("\n");
 		}
-		else if (Check == 2)
+		else if (check == 2)
 		{
-			Check = 0;
-			int bangou = -1;
+			printf("2019年\n");
+			printf("\n");
 
-			auto itr = house.begin();
-
-			printf("[リスト要素の挿入]\n");
-			printf("要素を追加する場所を指定してください。最後尾に追加する場合は999を入力してください\n");
-
-			scanf_s("%d", &bangou);
-
-			if (bangou == 999) {
-
-				printf("追加する要素の値を入力してください\n");
-
-				scanf_s("%d", &bangou);
-
-				house.push_back(bangou);
-
-				printf("要素%dが最後尾に挿入されました\n\n", bangou);
-			}
-			else if (bangou > house.size())
+			for (auto itr = station.begin(); itr != station.end(); ++itr)
 			{
-				printf("要素数が多すぎます。\n");
-			}
-			else {
-				printf("追加する要素の値を入力してください\n");
-
-				scanf_s("%d", &Mode);
-
-				for (int i = 1; i < bangou; i++)
+				if (*itr == "tabata")
 				{
+					std::cout << *itr << "\n";
 					itr++;
+					itr = station.insert(itr, "nisinippori");
 				}
 
-				itr = house.insert(itr, Mode);
-
-				printf("要素%dが%d番目に挿入されました\n\n", Mode, bangou);
-			}
-		}
-		else if (Check == 3 && house.size() != 0)
-		{
-			Check = 0;
-			it = house.begin();
-
-			printf("[要素の編集]\n");
-			printf("編集したい要素の番号を指定してください。\n");
-
-			scanf_s("%d", &Mode);
-
-			if (Mode > house.size())
-			{
-				printf("%d番目の要素が見つかりませんでした。\n\n", Mode);
-			}
-			else
-			{
-				for (int i = 0; i < Mode; i++)
+				if (*itr == "takanawagw-towei")
 				{
-					it++;
+					station.erase(itr);
+					continue;
 				}
 
-				int Chenge = 0;
-
-				printf("%d番目の要素の変更する値を入力してください。\n", Mode);
-
-				scanf_s("%d", &Chenge);
-
-				replace(house.begin(), house.end(), *it, Chenge);
-
-				printf("%d番目の要素の値が%dに変更されました。\n", Mode, Chenge);
-
-				continue;
-
+				std::cout << *itr << "\n";
 			}
+
+			printf("\n");
 		}
-		else if (Check == 4 && house.size() != 0)
+		else if (check == 3)
 		{
-			Check = 0;
-			it = house.begin();
+			printf("2022年\n");
+			printf("\n");
 
-			printf("[要素の削除]\n");
-			printf("削除したい要素の番号を指定してください。\n");
-
-			scanf_s("%d", &Mode);
-
-			if (Mode > house.size())
+			for (auto itr = station.begin(); itr != station.end(); ++itr)
 			{
-				printf("%d番目の要素が見つかりませんでした。\n\n", Mode);
-			}
-			else
-			{
-				for (int i = 0; i < Mode; i++)
+				if (*itr == "tabata")
 				{
-					it++;
+					std::cout << *itr << "\n";
+					itr++;
+					itr = station.insert(itr, "nisinippori");
+					//itr->push_back(*"nisinippori");
 				}
 
-				printf("%d番目の要素%dを削除しました。\n", Mode,*it);
+				if (*itr == "tamati")
+				{
+					std::cout << *itr << "\n";
+					itr++;
+					itr = station.insert(itr, "takanawagw-towei");
+				}
 
-				house.erase(it);
-
-				continue;
-
+				std::cout << *itr << "\n";
 			}
+
+			printf("\n");
 		}
 	}
 
